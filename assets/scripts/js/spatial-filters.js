6450cf6a5f6919b8e4ae995a195d31c8e4f6b544
Mateus Targino <mateustargino95@gmail.com>
2018-08-19 14:19:39 -0300

    organizing the enviroment

--- assets/scripts/js/spatial-filters.js
+++ assets/scripts/js/spatial-filters.js
@@ -1,21 +1,33 @@
+document.getElementById('sobelX').onclick = function() {
+  this.disabled = true;
+  document.body.classList.add("loading");
 
-let imgElement = document.getElementById('imageSrc');
-let inputElement = document.getElementById('fileInput');
-inputElement.addEventListener('change', (e) => {
-  imgElement.src = URL.createObjectURL(e.target.files[0]);
-}, false);
-imgElement.onload = function() {
-  let src = cv.imread(imgElement);
+  let src = cv.imread('imageSrc');
   let dstx = new cv.Mat();
-  let dsty = new cv.Mat();
+  //convert to binary
   cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
-  // You can try more different parameters
   cv.Sobel(src, dstx, cv.CV_8U, 1, 0, 3, 1, 0, cv.BORDER_DEFAULT);
+
+  cv.imshow('outputCanvas', dstx);
+  src.delete();
+  dstx.delete();
+  this.disabled = false;
+  document.body.classList.remove("loading");
+};
+
+document.getElementById('sobelY').onclick = function() {
+  this.disabled = true;
+  document.body.classList.add("loading");
+
+  let src = cv.imread('imageSrc');
+  let dsty = new cv.Mat();
+  //convert to binary
+  cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
   cv.Sobel(src, dsty, cv.CV_8U, 0, 1, 3, 1, 0, cv.BORDER_DEFAULT);
-  // cv.Scharr(src, dstx, cv.CV_8U, 1, 0, 1, 0, cv.BORDER_DEFAULT);
-  // cv.Scharr(src, dsty, cv.CV_8U, 0, 1, 1, 0, cv.BORDER_DEFAULT);
-  cv.imshow('canvasInput', scr);
-  cv.imshow('canvasOutputx', dstx);
-  cv.imshow('canvasOutputy', dsty);
-  src.delete(); dstx.delete(); dsty.delete();
+
+  cv.imshow('outputCanvas', dsty);
+  src.delete();
+  dsty.delete();
+  this.disabled = false;
+  document.body.classList.remove("loading");
 };
---
+++ assets/scripts/js/util.js
@@ -0,0 +1,23 @@
+document.body.classList.add("loading");
+
+function onOpenCvReady() {
+  document.body.classList.remove("loading");
+}
+
+let imgElement = document.getElementById('imageSrc');
+let inputElement = document.getElementById('fileInput');
+
+inputElement.onchange = function(event) {
+  imgElement.src = URL.createObjectURL(event.target.files[0]);
+};
+
+imgElement.onload = function() {
+  let image = cv.imread(imgElement);
+  cv.imshow('outputCanvas', image);
+  image.delete();
+};
+
+document.getElementById('downloadButton').onclick = function() {
+  this.href = document.getElementById("outputCanvas").toDataURL();
+  this.download = "image.png";
+};
